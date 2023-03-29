12d8a448bff825d24338dde9d65137fd229c6475
Kry765 <krzysztofkleka91@gmail.com>
2023-03-29 23:57:46 +0200

    add nav

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,17 @@
+import { IconMenu2 } from '@tabler/icons-react';
 import React from 'react'
+import '../style/main.scss';
 export default class Main extends React.Component{
     render(){
         return(
-            <div>Get started</div>
+            <div>
+                <nav className='nav'>
+                    <IconMenu2 className='nav__burger-icon'/>                    
+                </nav>
+                <div className='menu-items'>
+                    
+                </div>
+            </div>
         );
     }
 }
