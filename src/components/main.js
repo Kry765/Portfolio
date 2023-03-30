682802e7d10912f9ae57d8c5a27dec2fa1beebe8
Kry765 <krzysztofkleka91@gmail.com>
2023-03-30 23:32:00 +0200

    modified nav

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,26 @@ import React from 'react'
 import '../style/main.scss';
 export default class Main extends React.Component{
     render(){
+        const viewMenu = () => {
+            const x = document.querySelector('toggle-mobile-nav')
+            const y = document.querySelector('.nav__mobile-menu-items')
+            console.log(x)
+        }
+
         return(
             <div>
                 <nav className='nav'>
-                    <IconMenu2 className='nav__burger-icon'/>                    
+                    <button onClick={viewMenu}>
+                    <IconMenu2 className='nav__burger-icon' />    
+                    </button>
+                    <div className='nav__mobile-menu-items'>
+                        <div className='nav__mobile-menu-item'>About Me</div>
+                        <div className='nav__mobile-menu-item'>Skills</div>
+                        <div className='nav__mobile-menu-item'>Certyfication</div>
+                        <div className='nav__mobile-menu-item'>Concact me</div>
+                    </div>               
                 </nav>
-                <div className='menu-items'>
-                    
-                </div>
+                
             </div>
         );
     }
