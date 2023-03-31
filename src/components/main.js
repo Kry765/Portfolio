bb1bef30718d64f530274c4813429d98c7a7e9da
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-03-31 21:14:40 +0100

    modified nav

--- src/components/main.js
+++ src/components/main.js
@@ -1,29 +1,38 @@
-import { IconMenu2 } from '@tabler/icons-react';
+import { IconMenu2 } from '@tabler/icons-react'
 import React from 'react'
-import '../style/main.scss';
-export default class Main extends React.Component{
-    render(){
-        const viewMenu = () => {
-            const x = document.querySelector('toggle-mobile-nav')
-            const y = document.querySelector('.nav__mobile-menu-items')
-            console.log(x)
-        }
+import '../style/main.scss'
+export default class Main extends React.Component {
+	state = { isActive: false }
 
-        return(
-            <div>
-                <nav className='nav'>
-                    <button onClick={viewMenu}>
-                    <IconMenu2 className='nav__burger-icon' />    
-                    </button>
-                    <div className='nav__mobile-menu-items'>
-                        <div className='nav__mobile-menu-item'>About Me</div>
-                        <div className='nav__mobile-menu-item'>Skills</div>
-                        <div className='nav__mobile-menu-item'>Certyfication</div>
-                        <div className='nav__mobile-menu-item'>Concact me</div>
-                    </div>               
-                </nav>
-                
-            </div>
-        );
-    }
+	ToggleClass = () => {
+		this.setState({ isActive: !this.state.isActive })
+	}
+
+	render() {
+		const isActive = this.state.isActive
+
+		return (
+			<div>
+				<nav className='nav'>
+					{/*DESKTOP NAV*/}
+					<div className='nav__desktop-nav-items'>
+						<div className='desktop-nav-items__desktop-menu-item'>About Me</div>
+						<div className='desktop-nav-items__desktop-menu-item'>Skills</div>
+						<div className='desktop-nav-items__desktop-menu-item'>Certyfication</div>
+						<div className='desktop-nav-items__desktop-menu-item'>Concact me</div>
+					</div>
+					{/*MOBILE NAV*/}
+					<button onClick={this.ToggleClass}>
+						<IconMenu2 className='nav__burger-icon' />
+					</button>
+					<div className={isActive ? 'nav__mobile-menu-items' : 'nav__mobile-menu-items--active'}>
+						<div className='nav__mobile-menu-item'>About Me</div>
+						<div className='nav__mobile-menu-item'>Skills</div>
+						<div className='nav__mobile-menu-item'>Certyfication</div>
+						<div className='nav__mobile-menu-item'>Concact me</div>
+					</div>
+				</nav>
+			</div>
+		)
+	}
 }
