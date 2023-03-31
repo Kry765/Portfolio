95f62899876070a09fe3eb32d29e248a3c746d9e
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-03-31 22:52:01 +0100

    modified nav

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import { IconMenu2 } from '@tabler/icons-react'
+import { IconBrandCodepen } from '@tabler/icons-react'
 import React from 'react'
 import '../style/main.scss'
 export default class Main extends React.Component {
@@ -12,25 +13,32 @@ export default class Main extends React.Component {
 		const isActive = this.state.isActive
 
 		return (
-			<div>
-				<nav className='nav'>
-					{/*DESKTOP NAV*/}
-					<div className='nav__desktop-menu-items'>
-						<a className='nav__desktop-menu-item'>About Me</a>
-						<a className='nav__desktop-menu-item'>Skills</a>
-						<a className='nav__desktop-menu-item'>Certyfication</a>
-						<a className='nav__desktop-menu-item'>Concact me</a>
-					</div>
-
-					{/*MOBILE NAV*/}
-					<button onClick={this.ToggleClass}>
-						<IconMenu2 className='nav__burger-icon' />
-					</button>
-					<div className={isActive ? 'nav__mobile-menu-items' : 'nav__mobile-menu-items--active'}>
-						<div className='nav__mobile-menu-item'>About Me</div>
-						<div className='nav__mobile-menu-item'>Skills</div>
-						<div className='nav__mobile-menu-item'>Certyfication</div>
-						<div className='nav__mobile-menu-item'>Concact me</div>
+			<div className='wrapper'>
+				<nav>
+					<div className='nav'>
+						{/*LOGO */}
+						<div>
+							<IconBrandCodepen className='nav__logo' />
+						</div>
+						{/*DESKTOP NAV*/}
+						<div className='nav__desktop-menu-items'>
+							<a className='nav__desktop-menu-item'>About Me</a>
+							<a className='nav__desktop-menu-item'>Skills</a>
+							<a className='nav__desktop-menu-item'>Certyfication</a>
+							<a className='nav__desktop-menu-item'>Concact me</a>
+						</div>
+						{/*MOBILE NAV*/}
+						<div>
+							<button onClick={this.ToggleClass}>
+								<IconMenu2 className='nav__burger-icon' />
+							</button>
+						</div>
+						<div className={isActive ? 'nav__mobile-menu-items ' : 'nav__mobile-menu-items--active nav__mobile-menu-items--animation'}>
+							<div className='nav__mobile-menu-item'>About Me</div>
+							<div className='nav__mobile-menu-item'>Skills</div>
+							<div className='nav__mobile-menu-item'>Certyfication</div>
+							<div className='nav__mobile-menu-item'>Concact me</div>
+						</div>
 					</div>
 				</nav>
 			</div>
