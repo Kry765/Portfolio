b195fe6ec1a1cb9aae3dee283b443abfc144858d
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-07 21:16:40 +0200

    new div

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,25 +7,32 @@ export default function Skills() {
 			<h1>Skills</h1>
 			<div className='skills__cards'>
 				<div className='skills__card'>
+					<div></div>
 					<p>PostgreSQL</p>
 				</div>
 				<div className='skills__card'>
+					<div></div>
 					<p>React</p>
 				</div>
 				<div className='skills__card'>
+					<div></div>
 					<p>JavaScript</p>
 				</div>
 				<div className='skills__card'>
+					<div></div>
 					<p>CSS</p>
 				</div>
 				<div className='skills__card'>
+					<div></div>
 					<p>SCSS</p>
 				</div>
 				<div className='skills__card'>
+					<div></div>
 					<p>HTML</p>
 				</div>
 				<div className='skills__card'>
-					<p>Node.js</p>
+					<div></div>
+					<p>Nodejs</p>
 				</div>
 			</div>
 		</div>
