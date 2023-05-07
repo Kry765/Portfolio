00d1fea149755695e2661986af52ccb17b121693
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-07 18:01:54 +0200

    add section about us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Menu from './components/Menu'
 import Header from './components/Header'
+import Aboutme from './components/Aboutme'
 
 function App() {
 	return (
 		<div className='App'>
 			<Menu />
 			<Header />
+			<Aboutme />
 		</div>
 	)
 }
---
+++ src/components/Aboutme.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import picture from '../img/me.png'
+import '../style/_aboutme.scss'
+import '../style/reset.scss'
+export default function Aboutme() {
+	return (
+		<header>
+			<div className='about-me'>
+				<h1>About</h1>
+				<div className='about-me__card'>
+					<div className='about-me__foto'>
+						<img src={picture} />
+					</div>
+					<div className='about-me__text'>
+						loremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremloremlorloremlorloremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremloremlorloremlorloremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremlore
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
