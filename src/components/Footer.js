a1c54d8891789f299adc4d076aac4c35d8f05436
Kry765 <krzysztofkleka91@gmail.com>
2023-05-08 23:59:33 +0200

    add icon footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,27 @@
 import '../style/reset.scss'
 import '../style/_footer.scss'
+import {SiFacebook} from 'react-icons/si'
+import {TiSocialLinkedinCircular} from 'react-icons/ti'
+import {AiFillGithub} from 'react-icons/ai'
+import {MdOutlineAlternateEmail} from 'react-icons/md'
 
 export default function Footer() {
 	return (
 		<div className='footer'>
+			<div className='footer__socialmedia-items'>
+				<div className='footer__socialmedia-item'><SiFacebook/></div>
+				<div className='footer__socialmedia-item'><TiSocialLinkedinCircular/></div>
+				<div className='footer__socialmedia-item'><AiFillGithub/></div>
+				<div className='footer__socialmedia-item'><MdOutlineAlternateEmail/></div>
+			</div>
 			<div>
 				<nav>
 				<ul className='footer__bottom-menu-items'>
-					<li className='footer__bottom-menu-item'>Home</li>
-					<li className='footer__bottom-menu-item'>About Me</li>
-					<li className='footer__bottom-menu-item'>Skills</li>
-					<li className='footer__bottom-menu-item'>Certyfication</li>
-					<li className='footer__bottom-menu-item'>Contact me</li>
+					<li className='footer__bottom-menu-item'><a href="#">Home</a></li>
+					<li className='footer__bottom-menu-item'><a href="#">About Me</a></li>
+					<li className='footer__bottom-menu-item'><a href="#">Skills</a></li>
+					<li className='footer__bottom-menu-item'><a href="#">Certyfication</a></li>
+					<li className='footer__bottom-menu-item'><a href="#">Contact me</a></li>
 				</ul>
 				</nav>
 			</div>
