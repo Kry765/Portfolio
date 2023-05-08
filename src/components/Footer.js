a780103c456ae8868c3587fb042fb74d0224f6f2
Kry765 <krzysztofkleka91@gmail.com>
2023-05-08 23:47:46 +0200

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,20 @@ import '../style/_footer.scss'
 export default function Footer() {
 	return (
 		<div className='footer'>
-			<h1>Footer</h1>
+			<div>
+				<nav>
+				<ul className='footer__bottom-menu-items'>
+					<li className='footer__bottom-menu-item'>Home</li>
+					<li className='footer__bottom-menu-item'>About Me</li>
+					<li className='footer__bottom-menu-item'>Skills</li>
+					<li className='footer__bottom-menu-item'>Certyfication</li>
+					<li className='footer__bottom-menu-item'>Contact me</li>
+				</ul>
+				</nav>
+			</div>
+			<div>
+				<p>Kry765 | &copy; All right reserved 2023</p>
+			</div>
 		</div>
 	)
 }
