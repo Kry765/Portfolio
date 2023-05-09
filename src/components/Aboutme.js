c5e5b1b2f1264441350801b969044b73246af693
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-09 22:35:24 +0200

    add new btn

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import picture from '../img/me.png'
+import picture from '../img/3255337.jpg'
 import '../style/_aboutme.scss'
 import '../style/reset.scss'
 export default function Aboutme() {
@@ -8,7 +8,7 @@ export default function Aboutme() {
 			<h1>About me</h1>
 			<div className='about-me__card'>
 				<div className='about-me__foto'>
-					<img src={picture} />
+					<img src={picture} className='about-me__fototest' />
 				</div>
 				<div className='about-me__text'>
 					loremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremloremlorloremlorloremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremloremlorloremlorloremloremloremmloremlorloremlormloremlorloremlorloremloremlorloremloremloremlorloremloremlore
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ export default function Hedaer() {
 		<div className='header'>
 			<h1 className='header__title'>Hi My name is Chris</h1>
 			<p className='header__text'>Thank you for being here, let me tell you something about myself</p>
-			<button className='header__button'>Let's Go</button>
+			<div>
+				<button className='header__button'>Let's Go</button>
+				<button className='header__button'>Contact Me</button>
+			</div>
 		</div>
 	)
 }
