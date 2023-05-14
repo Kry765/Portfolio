ed5fa25a2c23f459ec50315d0a1773c967714142
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-14 15:30:46 +0200

    add new color from the section menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ export default function Hedaer() {
 				</div>
 				<div>
 					<p className='header__text'>Thank you for being here, let me tell you something about myself</p>
-					<button className='header__button'>Let's Go</button>
+					<button className='header__button--awarded'>Let's Go</button>
 					<button className='header__button'>Contact Me</button>
 				</div>
 			</div>
