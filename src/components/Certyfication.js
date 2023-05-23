b7d5df75b68d7904bc54e6710f3e9f04a9ac7db8
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-23 08:24:48 +0200

    upgrqade section skills

--- src/components/Certyfication.js
+++ src/components/Certyfication.js
@@ -4,7 +4,15 @@ import '../style/_certyfication.scss'
 export default function Certyfication() {
 	return (
 		<div className='certyfication' id='Certyfication'>
-			<h1>Certyfication</h1>
+			<div>
+				<h1>Certyfication</h1>
+			</div>
+			<div className='cert__diplomas'>
+				<div className='cert_diploma'>
+					<div>?</div>
+					<p>English</p>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,6 +8,7 @@ import { DiJavascript } from 'react-icons/di'
 import { SiReact } from 'react-icons/si'
 import { FaNodeJs } from 'react-icons/fa'
 import { AiFillGithub } from 'react-icons/ai'
+import { SiBem } from 'react-icons/si'
 
 export default function Skills() {
 	return (
@@ -18,57 +19,82 @@ export default function Skills() {
 					<div>
 						<DiPostgresql className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--postgres'></div>
+					</div>
 					<p>PostgreSQL</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<SiReact className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--react'></div>
+					</div>
 					<p>React</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<DiJavascript className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--js'></div>
+					</div>
 					<p>JavaScript</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<IoLogoCss3 className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--css'></div>
+					</div>
 					<p>CSS</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<FaSass className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--scss'></div>
+					</div>
 					<p>SCSS</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<AiFillHtml5 className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--html'></div>
+					</div>
 					<p>HTML</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<AiFillGithub className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--github'></div>
+					</div>
 					<p>GitHub</p>
 				</div>
 				<div className='skills__card'>
 					<div>
 						<FaNodeJs className='skills__icon' />
 					</div>
-					<div className='skills__progress-bar'></div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--node'></div>
+					</div>
 					<p>Nodejs</p>
+				</div>
+				<div className='skills__card'>
+					<div>
+						<SiBem className='skills__icon' />
+					</div>
+					<div className='skills__progress-bar'>
+						<div className='skills__progress-bar--bem'></div>
+					</div>
+					<p>BEM</p>
 				</div>
 			</div>
 		</div>
