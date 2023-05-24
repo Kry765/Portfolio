95cb8d153003f31a4338a1fab4704b713762dd9b
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-24 23:05:26 +0200

    upgrade section cert

--- src/components/Certyfication.js
+++ src/components/Certyfication.js
@@ -1,5 +1,8 @@
 import '../style/reset.scss'
 import '../style/_certyfication.scss'
+import Udemy1 from '../img/cert_cz1.jpg'
+import Udemy2 from '../img/cert_cz.2.jpg'
+// import
 
 export default function Certyfication() {
 	return (
@@ -7,10 +10,21 @@ export default function Certyfication() {
 			<div>
 				<h1>Certyfication</h1>
 			</div>
-			<div className='cert__diplomas'>
-				<div className='cert_diploma'>
-					<div>?</div>
-					<p>English</p>
+			<div className='certyfication__diplomas'>
+				<div>
+					<h3>English</h3>
+					<div>
+						<p>Certyfication</p>
+					</div>
+				</div>
+				<div>
+					<h3>Udemy</h3>
+					<div>
+						<img src={Udemy1} className='certyfication__img-diploma' alt='certyfication' />
+					</div>
+					<div>
+						<img src={Udemy2} className='certyfication__img-diploma' alt='certyfication' />
+					</div>
 				</div>
 			</div>
 		</div>
