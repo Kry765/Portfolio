909da7883c0aa42bcc50a329e97592fa99c83a48
Kry765 <krzysztofkleka91@gmail.com>
2023-05-24 23:50:08 +0200

    upgrade section cert

--- src/components/Certyfication.js
+++ src/components/Certyfication.js
@@ -11,13 +11,13 @@ export default function Certyfication() {
 				<h1>Certyfication</h1>
 			</div>
 			<div className='certyfication__diplomas'>
-				<div>
+				<div className='certyfiaction_diploma'>
 					<h3>English</h3>
 					<div>
-						<p>Certyfication</p>
+					<img src={Udemy1} className='certyfication__img-diploma' alt='certyfication' />
 					</div>
 				</div>
-				<div>
+				<div className='certyfiaction_diploma'>
 					<h3>Udemy</h3>
 					<div>
 						<img src={Udemy1} className='certyfication__img-diploma' alt='certyfication' />
