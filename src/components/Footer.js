3d354a616b9b483b19c18d2180074289f1da1679
Kry765 <krzysztof.kleka@poczta.onet.pl>
2023-05-25 09:29:02 +0200

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,44 @@
 import '../style/reset.scss'
 import '../style/_footer.scss'
-import {SiFacebook} from 'react-icons/si'
-import {TiSocialLinkedinCircular} from 'react-icons/ti'
-import {AiFillGithub} from 'react-icons/ai'
-import {MdOutlineAlternateEmail} from 'react-icons/md'
+import { SiFacebook } from 'react-icons/si'
+import { TiSocialLinkedinCircular } from 'react-icons/ti'
+import { AiFillGithub } from 'react-icons/ai'
+import { MdOutlineAlternateEmail } from 'react-icons/md'
 
 export default function Footer() {
 	return (
 		<div className='footer'>
 			<div className='footer__socialmedia-items'>
-				<div className='footer__socialmedia-item'><SiFacebook/></div>
-				<div className='footer__socialmedia-item'><TiSocialLinkedinCircular/></div>
-				<div className='footer__socialmedia-item'><AiFillGithub/></div>
-				<div className='footer__socialmedia-item'><MdOutlineAlternateEmail/></div>
+				<div className='footer__socialmedia-item'>
+					<SiFacebook />
+				</div>
+				<div className='footer__socialmedia-item'>
+					<TiSocialLinkedinCircular />
+				</div>
+				<div className='footer__socialmedia-item'>
+					<AiFillGithub />
+				</div>
+				<div className='footer__socialmedia-item'>
+					<MdOutlineAlternateEmail />
+				</div>
 			</div>
 			<div>
-				<nav>
-				<ul className='footer__bottom-menu-items'>
-					<li className='footer__bottom-menu-item'><a href="#">Home</a></li>
-					<li className='footer__bottom-menu-item'><a href="#">About Me</a></li>
-					<li className='footer__bottom-menu-item'><a href="#">Skills</a></li>
-					<li className='footer__bottom-menu-item'><a href="#">Certyfication</a></li>
-					<li className='footer__bottom-menu-item'><a href="#">Contact me</a></li>
-				</ul>
+				<nav className='footer__bottom-menu-items'>
+					<a href='#' className='footer__bottom-menu-item'>
+						Home
+					</a>
+					<a href='#' className='footer__bottom-menu-item'>
+						About Me
+					</a>
+					<a href='#' className='footer__bottom-menu-item'>
+						Skills
+					</a>
+					<a href='#' className='footer__bottom-menu-item'>
+						Certyfication
+					</a>
+					<a href='#' className='footer__bottom-menu-item'>
+						Contact me
+					</a>
 				</nav>
 			</div>
 			<div>
